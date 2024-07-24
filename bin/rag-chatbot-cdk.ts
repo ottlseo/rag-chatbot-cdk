7da6a041f618387a7986241a7fd65f829693ab04
ottlseo <dotsi@ewhain.net>
2024-07-24 23:14:44 +0800

    Feat: combine sync & query stack into one stack

--- bin/rag-chatbot-cdk.ts
+++ bin/rag-chatbot-cdk.ts
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { SyncKnowledgeBaseStack } from '../lib/syncKnowledgeBaseStack/syncKnowledgeBaseStack';
-import { QueryKnowledgeBaseStack } from '../lib/queryKnowledgeBaseStack/queryKnowledgeBaseStack';
+import { KnowledgeBaseApiStack } from '../lib/knowledgeBaseApiStack/knowledgeBaseApiStack';
 import { WebStack } from '../lib/webStack/webStack';
 
 const STACK_PREFIX = "RAGChatBot";
@@ -10,24 +9,17 @@ const DEFAULT_REGION = "us-west-2";
 
 const app = new cdk.App();
 
-const syncStack = new SyncKnowledgeBaseStack(app, `${STACK_PREFIX}-SyncKnowledgeBaseStack`, {});
-
-const queryStack = new QueryKnowledgeBaseStack(app, `${STACK_PREFIX}-QueryKnowledgeBaseStack`, {
-    customKnowledgeBaseId: syncStack.CustomKnowledgeBaseId,
-    defaultKnowledgeBaseId: syncStack.DefaultKnowledgeBaseId,
-});
-queryStack.addDependency(syncStack);
+const apiStack = new KnowledgeBaseApiStack(app, `${STACK_PREFIX}-KnowledgeBaseApiStack`, {});
 
 const webStack = new WebStack(app, `${STACK_PREFIX}-WebStack`, {
-    api_url_base: queryStack.ApiGatewayEndpoint,
-    custom_file_bucket: syncStack.CustomFileBucketName,
-    default_file_bucket: syncStack.DefaultFileBucketName,
+    api_url_base: apiStack.ApiGatewayEndpoint,
+    custom_file_bucket: apiStack.CustomFileBucketName,
+    default_file_bucket: apiStack.DefaultFileBucketName,
     env: {
         account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
         region: DEFAULT_REGION,
       },
 });
-webStack.addDependency(syncStack);
-webStack.addDependency(queryStack);
+webStack.addDependency(apiStack);
 
 app.synth();
--- lib/knowledgeBaseApiStack/knowledgeBaseApiStack.ts
+++ lib/knowledgeBaseApiStack/knowledgeBaseApiStack.ts
@@ -1,15 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import { bedrock } from '@cdklabs/generative-ai-cdk-constructs';
 import { S3EventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 
-export class SyncKnowledgeBaseStack extends cdk.Stack {
-  public readonly CustomKnowledgeBaseId: string;
-  public readonly DefaultKnowledgeBaseId: string;
+export class KnowledgeBaseApiStack extends cdk.Stack {
   public readonly CustomFileBucketName: string;
   public readonly DefaultFileBucketName: string;
+  public readonly ApiGatewayEndpoint: string;
 
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -49,7 +49,7 @@ export class SyncKnowledgeBaseStack extends cdk.Stack {
     const lambdaIngestionJob = new lambda.Function(this, "IngestionJob", {
         runtime: lambda.Runtime.NODEJS_20_X,
         handler: "injestJobLambda.handler",
-        code: lambda.Code.fromAsset("./lib/syncKnowledgeBaseStack"),
+        code: lambda.Code.fromAsset("./lib/knowledgeBaseApiStack"),
         timeout: cdk.Duration.minutes(5),
         environment: {
           KNOWLEDGE_BASE_ID: knowledgeBase.knowledgeBaseId,
@@ -75,7 +75,7 @@ export class SyncKnowledgeBaseStack extends cdk.Stack {
     });
     // upload sample data in advance
     new cdk.aws_s3_deployment.BucketDeployment(this, 'DeployDefaultDoc', {
-      sources: [cdk.aws_s3_deployment.Source.asset('./lib/syncKnowledgeBaseStack/data')],
+      sources: [cdk.aws_s3_deployment.Source.asset('./lib/knowledgeBaseApiStack/data')],
       destinationBucket: bucketForDefaultDoc
     });
 
@@ -115,7 +115,7 @@ export class SyncKnowledgeBaseStack extends cdk.Stack {
     const lambdaIngestionJobForDefaultDoc = new lambda.Function(this, "IngestionJobForDefaultDoc", {
       runtime: lambda.Runtime.NODEJS_20_X,
       handler: "injestJobLambda.handler",
-      code: lambda.Code.fromAsset("./lib/syncKnowledgeBaseStack"),
+      code: lambda.Code.fromAsset("./lib/knowledgeBaseApiStack"),
       timeout: cdk.Duration.minutes(5),
       environment: {
         KNOWLEDGE_BASE_ID: knowledgeBaseForDefaultDoc.knowledgeBaseId,
@@ -132,11 +132,88 @@ export class SyncKnowledgeBaseStack extends cdk.Stack {
       })
     );
 
-    this.CustomKnowledgeBaseId = knowledgeBase.knowledgeBaseId;
-    this.DefaultKnowledgeBaseId = knowledgeBaseForDefaultDoc.knowledgeBaseId;
+    // Query lambda 
+    const bedrockAccessPolicy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ["bedrock:*"],
+      resources: ["*"],
+    }); 
+
+    const lambdaQuery = new lambda.Function(this, "Query", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: "queryLambda.handler",
+      code: lambda.Code.fromAsset("./lib/knowledgeBaseApiStack"),
+      timeout: cdk.Duration.minutes(5),
+      environment: {
+        KNOWLEDGE_BASE_ID: knowledgeBase.knowledgeBaseId,
+      },
+      initialPolicy: [bedrockAccessPolicy], 
+    });
+
+    const lambdaQueryForDefaultDoc = new lambda.Function(this, "QueryDefaultDoc", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: "queryLambda.handler",
+      code: lambda.Code.fromAsset("./lib/knowledgeBaseApiStack"),
+      timeout: cdk.Duration.minutes(5),
+      environment: {
+        KNOWLEDGE_BASE_ID: knowledgeBaseForDefaultDoc.knowledgeBaseId,
+      },
+      initialPolicy: [bedrockAccessPolicy], 
+    });
+
+  // API Gateway 생성
+  const api = new apigw.RestApi(this, "RAGAPI", {
+      restApiName: "RAGAPI",
+      description: "RAG API using Bedrock Knowledge Base created by CDK",
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigw.Cors.ALL_ORIGINS,
+        allowMethods: apigw.Cors.ALL_METHODS,
+        allowHeaders: apigw.Cors.DEFAULT_HEADERS,
+      },
+      endpointTypes: [apigw.EndpointType.REGIONAL], // endpoint 유형을 Regional로 설정
+    });
+
+    // CORS 허용 관련 설정 (통합 응답, 메서드 응답 정의)
+    const integrationResponse = {
+      proxy: false,
+      integrationResponses: [
+        {
+          statusCode: "200",
+          responseParameters: {
+            "method.response.header.Access-Control-Allow-Origin": "'*'",
+          },
+          responseTemplates: {
+            "application/json": ""
+          },
+        },
+      ],
+    };
+
+    const methodResponse = {
+      methodResponses: [
+        {
+          statusCode: "200",
+          responseParameters: {
+            "method.response.header.Access-Control-Allow-Origin": true, // Response headers를 Access-Control-Allow-Origin으로 설정
+          },
+          responseModels: {
+            "application/json": apigw.Model.EMPTY_MODEL, // Response body를 application/json으로 빈 값으로 설정
+          },
+        },
+      ],
+    };
+
+    // Lambda 함수를 API Gateway에 POST 메서드로 연결
+    const queryLambdaIntegration = new apigw.LambdaIntegration(lambdaQuery, integrationResponse);
+    const queryDefaultDocLambdaIntegration = new apigw.LambdaIntegration(lambdaQueryForDefaultDoc, integrationResponse);
+
+    api.root.addResource("custom").addMethod("POST", queryLambdaIntegration, methodResponse);
+    api.root.addResource("default").addMethod("POST", queryDefaultDocLambdaIntegration, methodResponse);
+
     this.CustomFileBucketName = bucket.bucketName;
     this.DefaultFileBucketName = bucketForDefaultDoc.bucketName;
-
+    this.ApiGatewayEndpoint = api.url;
+ 
     new cdk.CfnOutput(this, "dataSourceBucketName", {
       value: bucket.bucketName,
       description: "S3 bucket name for custom uploading data",
@@ -159,6 +236,12 @@ export class SyncKnowledgeBaseStack extends cdk.Stack {
       value: knowledgeBaseForDefaultDoc.knowledgeBaseId,
       description: "KnowledgeBase ID for default uploaded s3 datasource",
       exportName: "DefaultKnowledgeBaseId"
+    });
+
+    // API Gateway URL 출력
+    new cdk.CfnOutput(this, "ApiGatewayUrl", {
+      value: `${api.url}`,
+      description: "RAG API endpoint URL for Prod stage",
     });
   }
 };
